570f112085442d7ee791db39dd160eb92d13f35f
Ionut Carst <icarst@carst.me>
2014-05-06 11:33:12 +0200

    Update connect.js

--- baseExtensions/connect.js
+++ baseExtensions/connect.js
@@ -1,30 +1,17 @@
 var io = require('socket.io').listen(8888);
-
 var Room = io.of('/room').on('connection', function(socket) {
-        
 	var joinedRoom = null;
-	
 	socket.on('join room', function(data) {
-	    
 	    socket.join(data);
-	    
 	    joinedRoom = data;
-	    
 	    socket.emit('joined', "you've joined " + data);
-	    
 	    socket.broadcast.to(joinedRoom).send('someone joined room');
-	    
 	}); 
-	
 	socket.on('fromclient', function(data) {
-	    
 	    if (joinedRoom) {
-	        
 	        socket.broadcast.to(joinedRoom).send(data);
 	    } else {
-	        
-	        socket.send("you're not joined a room." + "select a room and then push join.");
+	        socket.send("you did not joined a room." + "select a room and then push join.");
 	    }
 	});
-
 });
