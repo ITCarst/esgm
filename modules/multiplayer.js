55296a6b7027571c87c570f7da9e1d6464988eb9
Ionut Carst <icarst@carst.me>
2014-05-06 11:32:08 +0200

    Update multiplayer.js

--- modules/multiplayer.js
+++ modules/multiplayer.js
@@ -1,12 +1,8 @@
 //scale game acording to screen size
 (function(esgm) {
 
-    var mediator = esgm.esgmMediator;
-    var ioPort = 8888;
-    var w = window.location;
-
+    var mediator = esgm.esgmMediator, ioPort = 8888, w = window.location;
     var socketIoURL = w.protocol + '//' + w.hostname + ':' + ioPort + '/socket.io/socket.io.js';
-
     var time = new Date().getTime(),
         userID = Math.round( time * Math.random() );
 
@@ -19,96 +15,27 @@
                 mediator.publish(event, args);
             },
             connect: function(args, callback) {
-                
-                //setup default room name
-                var mRoom = '/multiplayer.room';
-                //url for socket to connect
-                var url = w.protocol + '//' + w.hostname + ':' + ioPort + mRoom;
-                
-                var textSet = mediator.getElById(['multiplayer']);
-
-
-                var joined = false;
-                var room = io.connect(url);
+                var mRoom = '/multiplayer.room', ////setup default room name
+                    url = w.protocol + '//' + w.hostname + ':' + ioPort + mRoom, //url for socket to connect
+                    textSet = mediator.getElById(['multiplayer']),
+                    joined = false,
+                    room = io.connect(url);
 
                 room.on('connect', function() {
                     textSet.innerHTML = 'Connected';
                 }); 
 
                 room.on('joined', function(msg) {
-
-                    console.log('joined room ' + msg)
-
                     textSet.innerHTML = msg + 'from server';
 
                 });                
 
                 room.on('message', function(msg) {
-
                     textSet.innerHTML = msg + 'from server';
                     joined = true;
 
                 });
-                
                 room.emit('join room', 'test');
-
-
-                /*
-                var url = w.protocol + '//' + w.hostname + ':' + ioPort,
-                    socket = io.connect(url),
-                    room = 'multiplayer.room';
-
-                var textSet = mediator.getElById(['multiplayer']);
-
-                //connectig to multipalyer
-                socket.on('connecting', function () {
-                    textSet.innerHTML = 'Loading...';
-                })
-
-                socket.on('connect', function() {
-                    
-                    textSet.innerHTML = 'Connected';
-                    
-                    //send the username with comand prompt
-                    socket.emit('set.username', prompt('What\s your username?'));
-
-                    //reveive the event with the object holding the username
-                    socket.on('username.set', function(username) {
-                        if(callback && typeof callback === 'function') {
-                            callback();
-                        }
-                        //send back game connect with the user id and name
-                        socket.emit('game.connect', {
-                            userID : userID,
-                            username: username.username
-                        });
-                    })
-
-                    socket.on('game.connected', function() {
-                        
-                        //look for exisitng game
-                        socket.emit('room', room);
-
-                        console.log('set room');
-
-                        //join / create
-
-                        //notify user that it's the host or joiner
-
-                        //run game
-
-                        //socket.emit('join.game', 'multiplayer.room');
-
-                    })
-
-                    socket.on('message', function(data) {
-
-                       console.log('Incoming message:', data);
-
-                    });
-
-                })
-                */
             }
         }
         return {
@@ -131,15 +58,3 @@
     esgm.fn.createScripts(socketIoURL, function(){})
 
 }(window.esgm));
-
-
-
-
-/*
-var clients = {},
-    url = w.protocol + '//' + w.hostname + ':' + ioPort,
-    socket = io.connect(url);
-
-socket.emit('game.connect', {
-    id: userID
-});*/
